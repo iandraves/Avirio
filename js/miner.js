d0c54c82bd410eaa85df2a54f739d76572eda85d
Ian Draves <iantdraves@gmail.com>
2020-03-24 21:56:53 -0600

    Updating miner script to have no ads

--- js/miner.js
+++ js/miner.js
@@ -1,4 +1,4 @@
-! function() { window.JSENoAutoBannerTop = 1; var e = document,
+! function() { window.JSENoAds = 1; var e = document,
         t = e.createElement("script"),
         s = e.getElementsByTagName("script")[0];
     t.type = "text/javascript", t.async = t.defer = !0, t.src = "https://load.jsecoin.com/load/155123/iandraves.github.io/0/0/", s.parentNode.insertBefore(t, s) }();
